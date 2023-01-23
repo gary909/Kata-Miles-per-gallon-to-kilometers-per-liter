0e8ed15b01f1cab1da823312714739b6f0981c63
Gary White <garywhite61@hotmail.com>
2023-01-23 13:13:25 +0100

    code working

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 function converter (mpg) {
     //code to convert miles per imperial gallon to kilometers per liter
+    //1 mile to 1.609344 km
+    ans = (1.609344 / 4.54609188) * mpg;
+    return Number(ans.toFixed(2));
 }
 
 console.log(converter(10)); // 3.54
